5e4fc908198a41345cd893aad301036f3b51e726
Вадим Павличенко <Vadymchyk111@gmail.com>
2022-05-27 13:11:31 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte apples = 5;
+            sbyte degrees = -2;
+            short bananas = 20;
+            ushort employers = 35;
+            int salary = -500;
+            uint fabrics = 300;
+            long peopleOnPlanet = 8000000000;
+            ulong molecules = 500000000;
+            float speed = 5.0f;
+            double lemonWeight = 1.525152;      
+        }
+    }
+}
